47aa6942dfa851a32b00b6e44e4f3b91c56384ac
Jessica <adwoaaman45@gmail.com>
2024-07-08 16:39:05 +0000

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <div>
         <div className='py-4 px-16'>
-        <nav className='flex justify-between'>
+        <nav className='flex justify-between items-center'>
           <Link to='/'><h1 className='text-2xl border-4 border-primary p-2 font-bold '>JO</h1></Link>
-          <div className='space-x-4 font-semibold'>
+          <div className='space-x-4 font-semibold items-center'>
             <a href='/'>Home</a>
             <a href='/projects'>Projects</a>
             <a href='/about'>About</a>
+           
+          </div>
+          <div className='flex gap-4'>
+            <a href='https://github.com/Amankwaa97'><FaGithub size={40} /></a>
+            <a href='https://www.linkedin.com/in/jessica-oppong-1560ab30b/'><FaLinkedin size={40} /></a>
           </div>
         </nav>
       </div>
